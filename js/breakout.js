55189fb6b58b10fdea2b0ee1860c6df76639e5f7
Matthew Rodriguez <142768608+mattrodriguez004@users.noreply.github.com>
2024-03-19 17:07:29 +0000

    Tue, Mar 19, 2024, 10:07 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -1,6 +1,12 @@
 
+rulesBtn = document.getElementById('rules-btn')
+rules = document.getElementById('rules')
+closeBtn = document.getElementById('close-btn')
 
+rulesBtn.addEventListener('click', () => {
+    rules.classList.add('show')
+})
 
-toggle.addEventListener('click', () => {
-    document.body.classList.toggle('rules')
+closeBtn.addEventListener('click', () => {
+    rules.classList.remove('show')
 })
