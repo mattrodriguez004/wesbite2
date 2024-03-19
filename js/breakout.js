6111dc798b2e6b96dca5bad45f7f0e6135e6daca
Matthew Rodriguez <142768608+mattrodriguez004@users.noreply.github.com>
2024-03-19 17:18:29 +0000

    Tue, Mar 19, 2024, 10:18 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -4,6 +4,20 @@ rules = document.getElementById('rules')
 closeBtn = document.getElementById('close-btn')
 ctx = canvas.getContext("2d")
 
+//create ball properties
+ball = {
+    x: canvas.width / 2,
+    y: canvas.height/2 ,
+    size: 10,
+    speed: 4,
+    dx: 4;
+    dy: -4;
+
+}
+
+// draw ball on canvas
+function drawball()
+
 rulesBtn.addEventListener('click', () => {
     rules.classList.add('show')
 })
