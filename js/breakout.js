e5af97afb93416504690b34c9e470d95bf55e94b
Matthew Rodriguez <142768608+mattrodriguez004@users.noreply.github.com>
2024-04-02 17:10:33 +0000

    Tue, Apr 2, 2024, 10:10 AM -07:00

--- js/breakout.js
+++ js/breakout.js
@@ -143,6 +143,7 @@ bricks.forEach(column => {
             if (
                 ball.x - ball.size > brick.x && //left brickside
                 ball.x + ball.size < brick.x + brick.w && //right
+                ball.y + ball.size > brick.y && /top
                 ball.y - ball.size < brick.y + brick.h //bottom
             ){
             ball.dy = -1 * ball.dy
