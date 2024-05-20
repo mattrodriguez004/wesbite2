bcc1e1aca68ada4dd9036b75d3c3e0bb3999f4c5
Matthew Rodriguez <142768608+mattrodriguez004@users.noreply.github.com>
2024-05-20 02:30:31 +0000

    Sun, May 19, 2024, 7:30 PM -07:00

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@
 button = document.getElementById('start-button')
 ds = document.getElementById('ds')
 
-
 button.addEventListener('click', () => {
 button.classList.add('invisible')
 stuff.classList.add('visible')
